40017f7bed524573721c62e791735c14e5148164
muneermoosa <muneermoosa.mca@gmail.com>
2023-11-10 11:25:01 +0530

    shooper first

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
      <Routes>
       <Route>
       <Route path='/' element={<Shop/>}/>
+      <Route path='/SHOPPER' element={<Shop/>}/>
       <Route path='/mens'element={<ShopCategory banner={men_banner} category="men"/>}/>
       <Route path='/womens'element={<ShopCategory banner={women_banner}category="women"/>}/>
       <Route path='/kids'element={<ShopCategory banner={kid_banner}category="kid"/>}/>
