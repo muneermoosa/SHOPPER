5b71309fbcd3f93d3bd3ac8f2823e5a4b524b5be
muneermoosa <muneermoosa.mca@gmail.com>
2023-11-10 11:41:56 +0530

    shooper

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
      <Navbar/>
      <Routes>
       <Route>
-      <Route path='/' element={<Shop/>}/>
-      <Route path='/SHOPPER' element={<Shop/>}/>
+      
+      <Route path='/SHOPPER'element={<Shop/>}/>
       <Route path='/mens'element={<ShopCategory banner={men_banner} category="men"/>}/>
       <Route path='/womens'element={<ShopCategory banner={women_banner}category="women"/>}/>
       <Route path='/kids'element={<ShopCategory banner={kid_banner}category="kid"/>}/>
